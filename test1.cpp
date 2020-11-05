8e3d269967afc38c5a00dbd5f328eb478959d070
Nguyen Nam <namnguyenphuong215@gmail.com>
2020-11-05 08:04:35 +0700

    sang t5

--- test1.cpp
+++ test1.cpp
@@ -1,24 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int TangMax(int a[],int n){
-    int b[n];
-    b[0]=1;
-    for(int i=1;i<n;i++){
-        b[i]=1;
-        for(int j=0;j<i;j++){
-            if(a[i]>a[j] && b[i]<b[j]+1)
-                b[i]=b[j]+1;
-        }
-    }
-
-    return *max_element(b,b+n);
-}
+int n,a[1005],dp[1005];
 int main(){
-    int t;cin>>t;
-    while(t--){
-        int n;cin>>n;
-        int a[n+1];
-        for(int i=0;i<n;i++) cin>>a[i];
-        cout<<TangMax(a,n)<<endl;
-    }
+	int t;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		for(int i=1;i<=n;i++){
+			cin>>a[i];
+			dp[i]=1;
+		}
+		int res=0;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<i;j++){
+				if(a[i]>a[j]) dp[i]=max(dp[i],dp[j]+1);
+			}
+			res=max(dp[i],res);
+		}
+		cout<<res<<endl;
+	}
 }
